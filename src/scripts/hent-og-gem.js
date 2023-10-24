e88e7dcfc5436e1af6536fa988e25ed1b46d6808
Brian Emilius <brian@brianemilius.com>
2023-10-24 13:50:45 +0200

    add more stuff

---
+++ src/scripts/hent-og-gem.js
@@ -0,0 +1,15 @@
+export default (async function() {
+	const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+	const pokemon = await response.json()
+
+	localStorage.setItem("pokemon", JSON.stringify(pokemon))
+
+	const savedPokemon = JSON.parse(localStorage.getItem("pokemon"))
+
+	console.log(savedPokemon)
+
+	document.querySelector(".hentoggem").innerHTML = `
+		<h1>${savedPokemon.name}</h1>
+		<p>weight: ${savedPokemon.weight}</p>
+	`
+})()
